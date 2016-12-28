a75d5b7040f0e8f639b84dfb9acc206b97dd56fb
ingikim <gokim365@gmail.com>
2016-12-28 18:48:27 +0900

    Week1

---
+++ 01-firstReverse.js
@@ -0,0 +1,10 @@
+/** 
+ * Using the JavaScript language, have the function FirstReverse(str) 
+ * take the str parameter being passed and return the string in reversed order.
+
+ * example 1: firstReverse("codestates"); => "setatsedoc"
+ * example 2: firstReverse("I love codestates"); => "setatsedoc evol I"
+ */
+
+var firstReverse = function(str) { 
+};
---
+++ 03-removeChar.js
@@ -0,0 +1,8 @@
+// It's pretty straightforward. Your goal is to create a function 
+// that removes the first and last characters of a string. 
+// You're given one parameter.
+
+function removeChar(str){
+ //You got this!
+
+};
