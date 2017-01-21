6088ea93fcf11e18cb03017f97f268ac51bfd794
taewoong <moondingie5@gmail.com>
2017-01-21 10:06:02 +0900

    Week 3

---
+++ 11-isIsogram.js
@@ -0,0 +1,12 @@
+/** 
+ * An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+ * Implement a function that determines whether a string that contains only letters is an isogram. 
+ * Assume the empty string is an isogram. Ignore letter case.
+ * example 1: isIsogram("Dermatoglyphics"); => true
+ * example 2: isIsogram("aba"); => false
+ * example 3: isIsogram("moOse"); => false // -- ignore letter case
+ */
+
+var isIsogram = function(str) {
+  //Your code here
+};
---
+++ 12-alphabetPosition.js
@@ -0,0 +1,12 @@
+/**
+ * In this exercise, you are required to, given a string, 
+ * replace every letter with its position in the alphabet. 
+ * If anything in the text isn't a letter, ignore it 
+ * and don't return it. a being 1, b being 2, etc.
+ * example 1: alphabetPosition("The sunset sets at twelve o' clock."); => "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+ */
+
+var alphabetPosition = function(text) {
+  // Your code here
+  return text;
+};
